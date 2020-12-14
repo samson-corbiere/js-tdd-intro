059a52388fe65c6097ee5e4e1bfac5af15a31557
samson-corbiere <samson.corbiere@gmail.com>
2020-12-14 11:30:59 +0100

    add solution

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,3 +1,9 @@
+function capitalizeFirst(input) {
+  return input.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
+
 // capitalizeFirst.js
 const assert = require('assert');
 
